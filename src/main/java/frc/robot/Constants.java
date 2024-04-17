1d5b651f76fc2311cf02d5a96e9e00edada45983
chargedpika <alexmusiitwa@gmail.com>
2024-04-17 19:47:09 -0400

    Initial repository

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+public class Constants {
+    public static final class HardwareConstants {
+        public static final int kPDH = 1;
+
+        public static final int kLEFT_LB_DRIVE = 2;
+        public static final int kLEFT_RB_DRIVE = 3;
+        public static final int kLEFT_TB_DRIVE = 4;
+        public static final int kRIGHT_RB_DRIVE = 5;
+        public static final int kRIGHT_LB_DRIVE = 6;
+        public static final int kRIGHT_TB_DRIVE = 7;
+
+
+
+    }
+}
