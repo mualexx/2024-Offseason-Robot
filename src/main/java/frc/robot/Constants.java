d7f978136940651048afcf33b65794a6e7f3c3f8
chargedpika <alexmusiitwa@gmail.com>
2024-04-17 19:49:29 -0400

    night changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,14 +2,14 @@ package frc.robot;
 
 public class Constants {
     public static final class HardwareConstants {
-        public static final int kPDH = 1;
+        public static final int kPDH_CAN = 1;
 
-        public static final int kLEFT_LB_DRIVE = 2;
-        public static final int kLEFT_RB_DRIVE = 3;
-        public static final int kLEFT_TB_DRIVE = 4;
-        public static final int kRIGHT_RB_DRIVE = 5;
-        public static final int kRIGHT_LB_DRIVE = 6;
-        public static final int kRIGHT_TB_DRIVE = 7;
+        public static final int kLEFT_LB_DRIVE_CAN = 2;
+        public static final int kLEFT_RB_DRIVE_CAN = 3;
+        public static final int kLEFT_TB_DRIVE_CAN = 4;
+        public static final int kRIGHT_RB_DRIVE_CAN = 5;
+        public static final int kRIGHT_LB_DRIVE_CAN = 6;
+        public static final int kRIGHT_TB_DRIVE_CAN = 7;
 
 
 
